07b85aa911920711eea0372731bd2995a6765128
方楚星 <50919312+fcxfcx@users.noreply.github.com>
2021-02-01 21:59:09 +0800

    完成了可见手跟随碰撞手移动的代码
    1. 可见手有碰撞体，而碰撞手无碰撞体，这点需要分辨清楚，因为碰撞手的作用是“陷入”物体提供力反馈距离
    2. 由于模型的命名高度规范，可见手跟随移动的代码采取了高度复用的设计模式，具体可行度有待实验

---
+++ Assets/Scripts/Core/CoreLogic/TargetMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using UnityEngine;
+
+namespace Core.CoreLogic
+{
+    /// <summary>
+    ///该类用于可见手的各个关节向碰撞手的特定关节移动
+    /// </summary>
+    public class TargetMove : MonoBehaviour
+    {
+        private GameObject targetObject;
+        // Start is called before the first frame update
+        void Start()
+        {
+            targetObject = GameObject.Find(GetTargetName());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var transform1 = transform;
+            transform1.position = targetObject.transform.position;
+            transform1.rotation = targetObject.transform.rotation;
+        }
+
+        /// <summary>
+        /// 获取当前物体的目标物体名
+        /// </summary>
+        /// <returns></returns>
+        private string GetTargetName()
+        {
+            string thisName = this.name;
+            string[] temp = thisName.Split('_');
+            temp[0] = "CH";
+            StringBuilder targetName = new StringBuilder();
+            for (var i = 0; i < temp.Length; i++)
+            {
+                targetName.Append(temp[i]);
+                if (i != temp.Length - 1)
+                {
+                    targetName.Append("_");
+                }
+            }
+
+            return targetName.ToString();
+        }
+    }
+}
